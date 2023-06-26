561629a7fb457b3643da0ea4e1516768d85bb557
bertekg <bartek.grywalski@gmail.com>
2023-06-26 21:53:03 +0200

    L20 Instalacja EF do projektu

---
+++ CarWorkshop.Infrastructure/Persistence/CarWorkshopDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CarWorkshop.Infrastructure.Persistence;
+public class CarWorkshopDbContext : DbContext
+{
+    public DbSet<Domain.Entities.CarWorkshop> CarWorkhops { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=CarWorkshopDb;Trusted_Connection=True;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Domain.Entities.CarWorkshop>()
+            .OwnsOne(c => c.ContactDetails);
+    }
+}
