04154ef96e154eb58bccf4189a2ec4a3ac93119a
bertekg <bartek.grywalski@gmail.com>
2023-07-03 20:35:06 +0200

    L51 Testy walidatora

---
+++ CarWorkshop.ApplicationTests/ApplicationUser/CurrentUserTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CarWorkshop.Application.ApplicationUser.Tests;
+
+public class CurrentUserTests
+{
+    [Fact()]
+    public void IsInRole_WithMatchinRoleSshouldReturnTrue()
+    {
+        // Arrange
+        CurrentUser currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Admin", "User"});
+
+        // Act
+        bool isInRole = currentUser.IsInRole("Admin");
+
+        // Assert
+        isInRole.Should().BeTrue();
+    }
+    [Fact()]
+    public void IsInRole_WithNonMatchinRole_shouldReturnFalse()
+    {
+        // Arrange
+        CurrentUser currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Admin", "User" });
+
+        // Act
+        bool isInRole = currentUser.IsInRole("Moderator");
+
+        // Assert
+        isInRole.Should().BeFalse();
+    }
+    [Fact()]
+    public void IsInRole_WithNonMatchinCaseRole_shouldReturnFalse()
+    {
+        // Arrange
+        CurrentUser currentUser = new CurrentUser("1", "test@tests.com", new List<string> { "Admin", "User" });
+
+        // Act
+        bool isInRole = currentUser.IsInRole("admin");
+
+        // Assert
+        isInRole.Should().BeFalse();
+    }
+}
---
+++ CarWorkshop.ApplicationTests/ApplicationUser/UserContextTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System.Security.Claims;
+using Xunit;
+
+namespace CarWorkshop.Application.ApplicationUser.Tests;
+
+public class UserContextTests
+{
+    [Fact()]
+    public void GetCurrentUser_WithAuthenticatedUser_ShouldReturnCurrentUser()
+    {
+        // Arrange
+        var claims = new List<Claim>
+        {
+            new Claim(ClaimTypes.NameIdentifier, "1"),
+            new Claim(ClaimTypes.Email, "test@tests.com"),
+            new Claim(ClaimTypes.Role, "Admin"),
+            new Claim(ClaimTypes.Role, "User")
+        };
+        var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Test"));
+        var httpContextAccessorMock = new Mock<IHttpContextAccessor>();
+        httpContextAccessorMock.Setup(x => x.HttpContext).Returns(new DefaultHttpContext()
+        {
+            User = user
+        });
+        UserContext userContext = new UserContext(httpContextAccessorMock.Object);
+
+        // Act
+        CurrentUser? currentUser = userContext.GetCurrentUser();
+
+        // Arrange
+        currentUser.Should().NotBeNull();
+        currentUser!.Id.Should().Be("1");
+        currentUser.Email.Should().Be("test@tests.com");
+        currentUser.Roles.Should().ContainInOrder("Admin", "User");
+    }
+}
