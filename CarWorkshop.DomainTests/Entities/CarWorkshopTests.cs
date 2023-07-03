f634c04e74d0882de5503b8db4a07c182ce624c3
bertekg <bartek.grywalski@gmail.com>
2023-07-03 15:15:40 +0200

    L48 CarWorkshop Tests

---
+++ CarWorkshop.DomainTests/Entities/CarWorkshopTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace CarWorkshop.Domain.Entities.Tests;
+
+public class CarWorkshopTests
+{
+    [Theory]
+    [InlineData("Test Workshop", "test-workshop")]
+    [InlineData("Warsztat", "warsztat")]
+    [InlineData("Super Warsztat Pana Marka 2", "super-warsztat-pana-marka-2")]
+    public void EncodeName_ForSetName_ShouldSetEncodedNam(string name, string encodedNameExpected)
+    {
+        // Arrange
+        var carWorkshop = new CarWorkshop
+        {
+            Name = name
+        };
+
+        // Act
+        carWorkshop.EncodeName();
+
+        // Assert
+        carWorkshop.EncodedName.Should().Be(encodedNameExpected);
+    }
+    [Fact()]
+    public void EncodeName_ForNullName_ShouldThrowException()
+    {
+        // Arrange
+        var carWorkshop = new CarWorkshop();
+
+        // Act
+        Action action = carWorkshop.EncodeName;
+
+        // Assert
+        action.Invoking(a => a.Invoke())
+            .Should().Throw<NullReferenceException>();
+    }
+}
